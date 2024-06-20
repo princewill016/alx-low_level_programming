7d7e9dda1e2b35d5283322a6f6c4483d335e150b
peter david <princewill016@gmail.com>
2024-06-20 06:06:32 -0700

    C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	write(2, MESSAGE, sizeof(MESSAGE) - 1);
+	return (1);
+}
