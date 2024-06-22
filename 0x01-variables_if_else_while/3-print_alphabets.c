6d0aaa4c411406b1dde938159cc914447da81411
peter david <princewill016@gmail.com>
2024-06-22 01:10:10 -0700

    m'

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,20 +12,14 @@
 int main(void)
 {
 	char letter;
-
-	/* Print lowercase alphabet */
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
-
-	/* Print uppercase alphabet */
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
-
-	putchar('\n');  /* Print newline */
-
+	putchar('\n'); 
 	return 0;
 }
