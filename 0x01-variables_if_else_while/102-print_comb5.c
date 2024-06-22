f5ea54f93a874145b3e2008290ee1752d88bddc7
peter david <princewill016@gmail.com>
2024-06-22 09:59:34 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,39 +11,31 @@
  */
 int main(void)
 {
-    int num1_tens, num1_ones, num2_tens, num2_ones;
+	int num1_tens, num1_ones, num2_tens, num2_ones;
 
-    for (num1_tens = 0; num1_tens <= 9; num1_tens++)
-    {
-        for (num1_ones = 0; num1_ones <= 9; num1_ones++)
-        {
-            for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
-            {
-                int start_ones = (num2_tens == num1_tens) ? num1_ones + 1 : 0;
-
-                for (num2_ones = start_ones; num2_ones <= 9; num2_ones++)
-                {
-                    // Printing first two-digit number
-                    putchar(num1_tens + '0');
-                    putchar(num1_ones + '0');
-                    putchar(' ');
-
-                    // Printing second two-digit number
-                    putchar(num2_tens + '0');
-                    putchar(num2_ones + '0');
-
-                    // Adding comma and space if not the last combination
-                    if (!(num1_tens == 9 && num1_ones == 9 && num2_tens == 9 && num2_ones == 9))
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	for (num1_tens = 0; num1_tens <= 9; num1_tens++)
+	{
+		for (num1_ones = 0; num1_ones <= 9; num1_ones++)
+		{
+			for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
+			{
+				int start_ones = (num2_tens == num1_tens) ? num1_ones + 1 : 0;
+				for (num2_ones = start_ones; num2_ones <= 9; num2_ones++)
+				{
+					putchar(num1_tens + '0');
+					putchar(num1_ones + '0');
+					putchar(' ');
+					putchar(num2_tens + '0');
+					putchar(num2_ones + '0');
+					if (!(num1_tens == 9 && num1_ones == 9 && num2_tens == 9 && num2_ones == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
