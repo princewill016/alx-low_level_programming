bd84d22c5396c7c6ef6275bd2032a8c223ffd0f9
peter david <princewill016@gmail.com>
2024-06-22 09:40:03 -0700

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,13 +16,12 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0'); /* Print the current number */
-        if (num < 9)
-	{
-		putchar(','); /* Print a comma */
-		putchar(' '); /* Print a space */
+		if (num < 9)
+		{
+			putchar(','); /* Print a comma */
+			putchar(' '); /* Print a space */
+		}
 	}
-	}
-
 	putchar('\n'); /* Print a newline at the end */
 
 	return (0);
