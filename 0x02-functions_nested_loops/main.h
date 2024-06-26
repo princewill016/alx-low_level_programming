fd3427757c57607e3e4fe5bd2e85255279cc359a
Peter david <proncewill016@gmail.com>
2024-06-26 14:25:02 -0700

    xx

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,7 @@
-#include main.h
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+int _putchar(char c);
 
-    return (0);
-}
+#endif /* MAIN_H */
 
