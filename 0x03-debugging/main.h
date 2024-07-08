2f7e7db5dbe61d127407b76ffdfc82fb28281bd2
Peter david <princewill016@gmail.com>
2024-07-07 20:01:19 -0700

    :(:

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>  // Include necessary standard header files here
+
+void positive_or_negative(int i);  // Declare the function prototype
+
+#endif /* MAIN_H */
