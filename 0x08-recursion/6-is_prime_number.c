da5e009fb6b21e34774ad9413432884f0942bd4c
Peter david <princewill016@gmail.com>
2024-07-10 05:36:14 -0700

    $))

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,6 +8,13 @@
  */
 int is_prime_number(int n)
 {
+    /* Edge cases */
+    if (n <= 1)
+    {
+        return (0); /* n is not a prime number */
+    }
+    
+    /* Helper function declaration */
     return (is_prime_helper(n, 2));
 }
 
@@ -16,4 +23,17 @@ int is_prime_number(int n)
  * @n: The number to check
  * @i: The current divisor to check if n is divisible by
  *
- * Return: 1 if n is a
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_helper(int n, int i)
+{
+    if (i * i > n)
+    {
+        return (1); /* n is a prime number */
+    }
+    if (n % i == 0)
+    {
+        return (0); /* n is not a prime number */
+    }
+    return (is_prime_helper(n, i + 1));
+}
