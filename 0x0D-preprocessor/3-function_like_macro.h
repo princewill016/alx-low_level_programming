65cb733d713a532a0c5af1bc894e9fe4079c6e18
Peter david <princewill016@gmail.com>
2024-07-22 22:36:47 -0700

    fdf'

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,1 @@
-#ifndef MACROS_H
-#define MACROS_H
-
 #define ABS(x) ((x) < 0 ? -(x) : (x))
-
-#endif /* MACROS_H */
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,1 @@
-#ifndef MACROS_H
-#define MACROS_H
-
 #define SUM(x, y) ((x) + (y))
-
-#endif /* MACROS_H */
