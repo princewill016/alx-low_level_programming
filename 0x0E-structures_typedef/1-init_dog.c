53903c45ccca76071c99c5a3b830590d140e5435
Peter david <princewill016@gmail.com>
2024-07-23 12:09:55 -0700

    ddf'

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner) {
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
