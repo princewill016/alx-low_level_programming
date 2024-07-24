46d954444b5ffef89ce5ebe697f568627bdc2eef
Peter david <princewill016@gmail.com>
2024-07-23 21:17:41 -0700

    ds

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("%s\n", __FILE__);
-    return 0;
+    return (0);
 }
