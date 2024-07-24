855324a1f09ad654a45a2c371dbf469fa1f92d0f
Peter david <princewill016@gmail.com>
2024-07-23 21:08:54 -0700

    dsd' git push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,9 +8,9 @@
  * @owner: The name of the dog's owner (char pointer)
  */
 typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
+	char *name;
+	float age;
+	char *owner;
+	} dog_t;
 
 #endif /* DOG_H_ */
