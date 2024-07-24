d9dae3be67efcc53d7048efa9bf214cc5049c535
Peter david <princewill016@gmail.com>
2024-07-23 21:06:04 -0700

    dsd' git push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#ifndef DOG_H_
+#define DOG_H_
 
-struct dog {
+/**
+ * struct dog - A structure to represent a dog
+ * @name: The name of the dog (char pointer)
+ * @age: The age of the dog (float)
+ * @owner: The name of the dog's owner (char pointer)
+ */
+typedef struct dog {
     char *name;
     float age;
     char *owner;
-};
+} dog_t;
+
+#endif /* DOG_H_ */
