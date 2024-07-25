d203d3b45ac2a17ceed45a436f4b856f23f02c1b
Peter david <princewill016@gmail.com>
2024-07-25 04:56:35 -0700

    dhfd

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-void print_name(char *name, void (*f)(char *));
-void _putchar(char c); 
+#include <stddef.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
