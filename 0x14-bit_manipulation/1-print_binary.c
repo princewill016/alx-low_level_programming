81cff88549f8953c40eb58a11ca418064c7c7f72
Peter david <princewill016@gmail.com>
2024-08-08 21:49:21 -0700

    fdd

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,11 +7,25 @@
 void print_binary(unsigned long int n)
 {
     unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+    int started = 0;
 
     while (mask)
     {
-        putchar((n & mask) ? '1' : '0');
+        if (n & mask)
+        {
+            putchar('1');
+            started = 1;
+        }
+        else if (started)
+        {
+            putchar('0');
+        }
         mask >>= 1;
+    }
+    
+    if (!started)
+    {
+        putchar('0');
     }
 }
 
