6aaa10293a13a00b3cf0712fcc75529ab57b7b91
Peter david <princewill016@gmail.com>
2024-08-08 21:28:58 -0700

    fdd

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,20 +9,20 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int diff;
-    unsigned int count;
+	unsigned long int diff;
+	unsigned int count;
 
     /* Calculate the XOR of n and m to get differing bits */
-    diff = n ^ m;
+	diff = n ^ m;
 
-    count = 0;
+	count = 0;
     /* Count the number of set bits in the result */
-    while (diff > 0)
-    {
-        count += diff & 1;  /* Increment count if the rightmost bit is set */
-        diff >>= 1;         /* Shift right to check the next bit */
-    }
+	while (diff > 0)
+	{
+	count += diff & 1;  /* Increment count if the rightmost bit is set */
+	diff >>= 1;         /* Shift right to check the next bit */
+	}
 
-    return (count);
+	return (count);
 }
 
