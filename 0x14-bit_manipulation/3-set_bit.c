009caa6b8a5cdd039d9e966e49153c51c4a69a33
Peter david <princewill016@gmail.com>
2024-08-08 21:36:28 -0700

    fdd

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,14 +10,14 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
     /* Check if the index is out of range */
-    if (index >= sizeof(unsigned long int) * 8)
-    {
-        return (-1);
-    }
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+	return (-1);
+	}
 
     /* Set the bit at the given index to 1 */
-    *n |= (1UL << index);
+	*n |= (1UL << index);
 
-    return (1);
+	return (1);
 }
 
