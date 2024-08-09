b91af5faec1b131bfd54af0b9f276f4746351a8f
Peter david <princewill016@gmail.com>
2024-08-08 21:40:20 -0700

    fdd

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,11 +12,12 @@ int get_bit(unsigned long int n, unsigned int index)
     /* Check if the index is out of range */
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-        return (-1);
+	return (-1);
 	}
 
     /* Shift the bit at the given index to the rightmost position and 
        check if it is set (1) or not (0) */
+
 	return ((n >> index) & 1);
 }
 
