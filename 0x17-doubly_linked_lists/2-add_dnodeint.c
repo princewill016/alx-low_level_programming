8b0658280b4e6e2ab1c4ff9bc86dfa5873bb3ae5
Peter david <princewill016@gmail.com>
2024-09-19 10:26:46 -0700

    fdf

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @head: double pointer to the head of the list
- * @n: the value to be added to the new node
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list
+ * @head: Double pointer to the head of the list
+ * @n: The value to store in the new node
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: The address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
@@ -16,12 +16,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     if (new_node == NULL)
         return (NULL);
 
-    /* Initialize the new node's data */
+    /* Initialize the new node */
     new_node->n = n;
-    new_node->prev = NULL; /* New node will be the first, so no previous node */
-    new_node->next = *head; /* Point to the current head of the list */
+    new_node->prev = NULL;
+    new_node->next = *head;
 
-    /* If the list is not empty, set the previous head's prev to the new node */
+    /* If the list is not empty, update the previous head node */
     if (*head != NULL)
         (*head)->prev = new_node;
 
