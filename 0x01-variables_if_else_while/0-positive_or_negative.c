8a277f0bc3c950ab7c032af0521c9e866ea3cba9
Peter david <princewill016@gmail.com>
2024-10-10 20:22:38 -0700

    first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,35 +1,31 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include "hash_tables.h"
 
 /**
- * main - Entry point
+ * hash_table_create - Creates a hash table.
+ * @size: Size of the array.
  *
- * Description: Generates a random number and determines if it is
- * positive, negative, or zero.
- *
- * Return: Always 0 (Success)
+ * Return: Pointer to the newly created hash table, or NULL if it fails.
  */
-int main(void)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	int n;
+    hash_table_t *new_table;
+    unsigned long int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    new_table = malloc(sizeof(hash_table_t));
+    if (new_table == NULL)
+        return (NULL);
 
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+    new_table->size = size;
+    new_table->array = malloc(sizeof(hash_node_t *) * size);
+    if (new_table->array == NULL)
+    {
+        free(new_table);
+        return (NULL);
+    }
 
-	return (0);
+    for (i = 0; i < size; i++)
+        new_table->array[i] = NULL;
+
+    return (new_table);
 }
-
