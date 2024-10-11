dea7204897ab4695ec5b2bf3ac926b2f1526e110
Peter david <princewill016@gmail.com>
2024-10-10 20:32:28 -0700

    potential change

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,37 +2,38 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table
- * @size: Size of the array
+ * hash_table_create - Creates a hash table.
+ * @size: Size of the array.
  *
- * Return: Pointer to the newly created hash table, NULL if something went wrong
+ * Return: Pointer to the newly created hash table,
+ * NULL if something went wrong.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int i;
+	hash_table_t *ht;
+	unsigned long int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    /* Allocate memory for the hash table structure */
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-    /* Allocate memory for the array of pointers */
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
+	/* Allocate memory for the array of pointers */
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
 
-    /* Initialize all elements to NULL */
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
+	/* Initialize all elements to NULL */
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    ht->size = size;
+	ht->size = size;
 
-    return (ht);
+	return (ht);
 }
